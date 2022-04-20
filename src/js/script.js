d39b30ba5f1f9ad50a8610453bc323468c50545c
levIsProj <103768919+levIsProj@users.noreply.github.com>
2022-04-20 12:54:05 +0500

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,91 @@
-new Swiper('.image-slider', {
-    navigation: {
-        nextE1: '.swiper-button-next',
-        prevE1: 'siper-button-prew'
+'use strict';
 
-    },
-    slideToClickedSlide: true,
+function Tabs() {
+    var bindAll = function () {
+        var menuElements = document.querySelectorAll('[data-tab]');
+        for (var i = 0; i < menuElements.length; i++) {
+            menuElements[i].addEventListener('click', change, false);
+        }
+    }
 
-    mousewheel: {
-        sensitivity: 2,
-    },
+    var clear = function () {
+        var menuElements = document.querySelectorAll('[data-tab]');
+        for (var i = 0; i < menuElements.length; i++) {
+            menuElements[i].classList.remove('active');
+            var id = menuElements[i].getAttribute('data-tab');
+            document.getElementById(id).classList.remove('active');
+        }
+    }
 
+    var change = function (e) {
+        clear();
+        e.target.classList.add('active');
+        var id = e.currentTarget.getAttribute('data-tab');
+        document.getElementById(id).classList.add('active');
+    }
+
+    bindAll();
+}
+
+var connectTabs = new Tabs();
+
+var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
+   slidesOffsetAfter:1620,
     spaceBetween: 30,
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: false
-        
-    },
-});
-
-new Swiper('.swiperPartner', {
-  
-    slideToClickedSlide: true,
-
     mousewheel: {
         sensitivity: 1,
     },
-
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-
-    scrollbar: {
-        el: '.swiper-scrollbar1',
-        hide: false
+    breakpoints: {
+		1920: {
+			slidesOffsetBefore:290,
+		},
+        1025: {
+			slidesOffsetBefore:280,
+		},
+		800: {
+			slidesOffsetBefore:120,
+		}
         
+	},
+    scrollbar:{
+        el:".swiper-scrollbar",
+        draggable:true,
+},
+});
+
+var slider = new Swiper(".swiperPartner",{
+    slidesPerView: 'auto',
+   slidesOffsetAfter:780,
+    spaceBetween: 30,
+    mousewheel: {
+        sensitivity: 1,
     },
+    breakpoints: {
+		1920: {
+			slidesOffsetBefore:292,
+		},
+        1025: {
+			slidesOffsetBefore:270,
+		},
+		800: {
+			slidesOffsetBefore:120,
+		},
+        
+        800: {
+			slidesOffsetBefore:120,
+		},
+	},
+    scrollbar:{
+        el:".swiper-scrollbar1",
+        draggable:true,
+},
 });
 
-
-$(".slider-no").on('click', function (event) {
-    $(event.currentTarget).toggleClass("slider-no1"),
-        $(".not").toggleClass("not1");
+$(".c").on('click', function (event) {
+    $(event.currentTarget).toggleClass("c1")
 });
-
-
-$("#1").on('click', function (event) {
+    $(".slider-AN").on('click', function (event) {
     $(event.currentTarget).toggleClass("slider-AN1"),
-        $("#1a").toggleClass("ant1");
-
-});
-
-$("#2").on('click', function (event) {
-    $(event.currentTarget).toggleClass("slider-AN1"),
-        $("#2a").toggleClass("ant1");
-
-});
-
-$("#3").on('click', function (event) {
-    $(event.currentTarget).toggleClass("slider-AN1"),
-        $("#3a").toggleClass("ant1");
-
+        $(".ant",event.currentTarget).toggleClass("ant1")
 });
